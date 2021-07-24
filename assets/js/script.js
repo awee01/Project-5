6f48460d8208aef265b5f4e2d973c627884601d2
awee01 <85651950+awee01@users.noreply.github.com>
2021-07-23 21:37:29 -0400

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,40 +6,32 @@ var currentDay = moment().format("dddd, MMMM Do");
 //display current date
 document.getElementById("currentDay").innerHTML = currentDay;
 
-$(document).ready(function () {
-
 //save button
 $(".saveBtn").on("click", function () {
 
   // get values
-  var text = $(this).siblings(".text-input").val();
-  var time = $(this).parent().attr("id");
-  localStorage.setItem(time, text);
+  var recordedInput = $(this).siblings(".text-input").val();
+  var recordedTime = $(this).parent().attr("id");
+  localStorage.setItem (recordedTime, recordedInput);
 
-  })
+})
 
-
-  $(".deleteBtn").on("click", function () {
-
-    $("text1").empty();
-  })
-
-function time() {
+//defining function to compare colors
+function timeComparison() {
 
   // current time
   var currentTime = moment().hour();
 
-
   $(".time-block").each(function () {
-    var scheduledTime = parseInt($(this).attr("id").split("hour")[1]);
+    var scheduledTime =($(this).attr("id"));
 
     // check to see if scheduled time is equal to current time
-    if (scheduledTime < currentTime) {
+      if (scheduledTime < currentTime) {
       $(this).removeClass("future");
       $(this).removeClass("present");
       $(this).addClass("past");
-    }
-    else if (scheduledTime === currentTime) {
+      }
+      else if (scheduledTime === currentTime) {
       $(this).removeClass("past");
       $(this).removeClass("future");
       $(this).addClass("present");
@@ -54,15 +46,16 @@ function time() {
   }
 
   // Retrieving saved items from local storage
-  $("#hour9 .text-input").val(localStorage.getItem("hour9"));
-  $("#hour10 .text-input").val(localStorage.getItem("hour10"));
-  $("#hour11 .text-input").val(localStorage.getItem("hour11"));
-  $("#hour12 .text-input").val(localStorage.getItem("hour12"));
-  $("#hour13 .text-input").val(localStorage.getItem("hour13"));
-  $("#hour14 .text-input").val(localStorage.getItem("hour14"));
-  $("#hour15 .text-input").val(localStorage.getItem("hour15"));
-  $("#hour16 .text-input").val(localStorage.getItem("hour16"));
-  $("#hour17 .text-input").val(localStorage.getItem("hour17"));
+  $("#9 .text-input").val(localStorage.getItem("9"));
+  $("#10 .text-input").val(localStorage.getItem("10"));
+  $("#11 .text-input").val(localStorage.getItem("11"));
+  $("#12 .text-input").val(localStorage.getItem("12"));
+  $("#13 .text-input").val(localStorage.getItem("13"));
+  $("#14 .text-input").val(localStorage.getItem("14"));
+  $("#15 .text-input").val(localStorage.getItem("15"));
+  $("#16 .text-input").val(localStorage.getItem("16"));
+  $("#17 .text-input").val(localStorage.getItem("17"));
 
-  time();
-})
+
+  // Execute time function
+  timeComparison();
